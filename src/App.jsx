b639828cbd704edbc392ad3bccee2fc512a38c7f
Yousef Saad <saadrslan95@gmail.com>
2024-09-05 18:06:15 +0100

    Hero section buliding

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -5,14 +6,7 @@ export default function App() {
   <div className="min-h-screen bg-gray-100">
    
       <Navbar />
-
-     
-      <main className="p-4 md:p-8 lg:p-12">
-        <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-center text-[#37003c]">
-          Welcome to Fantasy Football AI
-        </h1>
-        {/* Your app's content goes here */}
-      </main>
+      <Hero />
     </div>
   );
 }
